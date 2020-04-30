3618a26132f3cdb9f5da5475f666991598db94f9
chashikajw <chashikajw007@gmail.com>
2020-05-01 01:39:44 +0530

    implement mgwswagger creation

--- config_generator.go
+++ config_generator.go
@@ -1,2 +1,73 @@
 package main
+
+import (
+
+	"envoy-config-generator/utills"
+	"envoy-config-generator/models/apiDefinition"
+
+	"encoding/json"
+
+	"fmt"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/go-openapi/spec"
+	"io/ioutil"
+	"os"
+
+
+
+)
+
 //package envoy_config_generator
+
+func main() {
+	mg := GenerateMgwSwagger()
+	fmt.Println(mg)
+}
+
+func GenerateMgwSwagger() apiDefinition.MgwSwagger {
+	var mgwSwagger  apiDefinition.MgwSwagger
+
+	// Open our jsonFile
+	openApif, err := os.Open("test.yaml")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened open api file")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer openApif.Close()
+
+	// read our opened jsonFile as a byte array.
+	jsn, _ := ioutil.ReadAll(openApif)
+
+	apiJsn, err := utills.ToJSON(jsn)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+
+	swaggerVerison, err := utills.FindSwaggerVersion(apiJsn)
+
+	if(swaggerVerison == "2") {
+		//map json to struct
+		var ApiData spec.Swagger
+		err = json.Unmarshal(apiJsn, &ApiData)
+		if err != nil {
+			fmt.Printf("openAPI unmarsheliing err: %v\n", err)
+		} else {
+			mgwSwagger.SetInfoSwagger(ApiData)
+		}
+
+	}else if(swaggerVerison == "3") {
+		//map json to struct
+		var ApiData openapi3.Swagger
+		err = json.Unmarshal(apiJsn, &ApiData)
+		if err != nil {
+			fmt.Printf("openAPI unmarsheliing err: %v\n", err)
+		} else {
+			mgwSwagger.SetInfoOpenApi(ApiData)
+		}
+	}
+
+	return mgwSwagger
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module envoy-config-generator
 
 go 1.14
+
+require (
+	github.com/envoyproxy/go-control-plane v0.9.5
+	github.com/getkin/kin-openapi v0.8.0
+	github.com/ghodss/yaml v1.0.0
+	github.com/go-openapi/spec v0.19.7
+)
